4450b3a9232a566222e072dc8403e1f97e7723e5
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-12-02 11:57:23 +0600

    first commit

---
+++ src/shared/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className='md:flex bg-white py-4'>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
